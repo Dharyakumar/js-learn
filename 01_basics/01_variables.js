4d182e3ed45f5eb57eea565bd9c94caeeb2b58a1
Dharya Kumar <171420485+Dharyakumar@users.noreply.github.com>
2025-08-14 07:38:14 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "dharya@google.com"
+var accountPassword = "12345"
+accountCity = "Gumla"
+
+//accountId = 2
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity])
